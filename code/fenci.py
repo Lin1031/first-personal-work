dfc5e471cc9bfa06bd7d30e5843ab511b69ab91c
Lin1031 <LinJH1031@outlook.com>
2021-02-17 22:01:39 +0800

    Update total number of participles

--- code/fenci.py
+++ code/fenci.py
@@ -54,7 +54,7 @@ def main():
     for i in range(len(items)):
         CountDict = {}
         word, count = items[i]
-        if count >= 50:
+        if count >= 25:
             CountDict["name"] = word
             CountDict["value"] = count
             CountList.append(CountDict)
